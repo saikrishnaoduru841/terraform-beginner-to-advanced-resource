6e46d19e5ac75e4df42b682e6a159fab0fee6766
saikrishnaoduru841 <89731382+saikrishnaoduru841@users.noreply.github.com>
2022-12-01 10:33:53 +0530

    Update attributes.tf
    commit

--- Section 2 - Read, Generate, Modify Congiruations/attributes.tf
+++ Section 2 - Read, Generate, Modify Congiruations/attributes.tf
@@ -9,7 +9,7 @@ resource "aws_eip" "lb" {
 }
 
 output "eip" {
-  value = aws_eip.lb
+  value = aws_eip.lb.public_ip
 }
 
 resource "aws_s3_bucket" "mys3" {
@@ -17,5 +17,5 @@ resource "aws_s3_bucket" "mys3" {
 }
 
 output "mys3bucket" {
-  value = aws_s3_bucket.mys3
+  value = aws_s3_bucket.mys3.bucket_domain_name
 }
